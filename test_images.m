0986114aa780b7080d24a4a0a2d3c5bb17026f85
czymadeinfuture <129965676+czymadeinfuture@users.noreply.github.com>
2024-11-07 09:19:48 +0100

    tester et obtenir le resultat en forme json

---
+++ test_images.m
@@ -0,0 +1,46 @@
+close all;
+
+testImageFolder = './images/test-resized2'; 
+
+categoriesTrain = (0:79);
+
+% read the test-images
+imageFiles = dir(fullfile(testImageFolder, '*.jpg')); 
+imageNames = string(fullfile(testImageFolder, {imageFiles.name}));
+
+thresholdValue = 0.5;
+
+figure
+tiledlayout(1,5)
+results = containers.Map();
+
+for i = 1:5
+    img = imread(imageNames(i));
+    img = imresize(img,inputSize(1:2));
+
+    scoresImg = predict(trainedNet,single(img))';
+    % disp(scoresImg)
+    YPred =  categoriesTrain(scoresImg >= thresholdValue) ;
+
+    [~, imageName, ~] = fileparts(imageFiles(i).name);
+    results(imageName) = YPred;
+
+    nexttile
+    imshow(img)
+    title(YPred)
+
+end
+
+jsonData = jsonencode((results),PrettyPrint=true);
+
+
+% save the result
+jsonFileName = 'predicted_labels1.json';
+fid = fopen(jsonFileName, 'w');
+if fid == -1
+    error('Can not create');
+end
+fwrite(fid, jsonData, 'char');
+fclose(fid);
+
+disp(['Already save', jsonFileName]);
