831e2069bd092972397f874548a1a1d693da4941
Rolgarel <83459917+Rolgarel@users.noreply.github.com>
2024-11-09 23:02:12 +0100

    ajout d'initialisation de plusieurs networks
    réseaux disponibles :
     - resnet18
     - resnet50
     - resnet101
     - mobilenetv2
    réalisation de test sur model réduit (5000)
    (voir onglet test du rapport)

--- modele1.m
+++ modele1.m
@@ -1,24 +1,32 @@
 close all;
 
-%net = imagePretrainedNetwork("resnet50",NumClasses=numClasses);
-net = imagePretrainedNetwork("resnet18",NumClasses=numClasses);
-
 layer = sigmoidLayer('Name', 'sig1');
 
-net = replaceLayer(net,"prob",layer);
+net = imagePretrainedNetwork("mobilenetv2",NumClasses=numClasses);
+net = replaceLayer(net,"Logits_softmax",layer);
 
-analyzeNetwork(net);
+%net = imagePretrainedNetwork("resnet18",NumClasses=numClasses);
+%net = replaceLayer(net,"prob",layer);
 
-% net = freezeNetwork(net,LayerNamesToIgnore="Logits"); % pour mobilenetv2
-net = freezeNetwork(net,LayerNamesToIgnore="fc1000"); % pour resnet18/resnet50
+%net = imagePretrainedNetwork("resnet50",NumClasses=numClasses);
+%net = replaceLayer(net,"fc1000_softmax",layer);
+
+%net = imagePretrainedNetwork("resnet101",NumClasses=numClasses);
+%net = replaceLayer(net,"prob",layer);
+
+%analyzeNetwork(net);
+
+%net = freezeNetwork(net,LayerNamesToIgnore="Logits"); % pour mobilenetv2
+%net = freezeNetwork(net,LayerNamesToIgnore="fc1000"); % pour resnet18/resnet50/resnet101
+
 
 options = trainingOptions("adam", ...
     InitialLearnRate=0.01, ...
-    MiniBatchSize=32, ...
-    MaxEpochs= 1, ...
+    MiniBatchSize=64, ...
+    MaxEpochs= 10, ...
     Verbose= false, ...
     ValidationData=valData, ...
-    ValidationFrequency=100, ...
+    ValidationFrequency=10, ...
     ValidationPatience=5, ...
     Metrics="accuracy", ...
     Plots="training-progress");
