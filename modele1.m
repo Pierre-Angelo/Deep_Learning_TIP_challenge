4011442203443c64961e3b376b8e848ef4718a6f
Rolgarel <83459917+Rolgarel@users.noreply.github.com>
2024-11-23 23:30:28 +0100

    param

--- modele1.m
+++ modele1.m
@@ -2,8 +2,8 @@ close all;
 
 layer = sigmoidLayer('Name', 'sig1');
 
-net = imagePretrainedNetwork("mobilenetv2",NumClasses=numClasses);
-net = replaceLayer(net,"Logits_softmax",layer);
+%net = imagePretrainedNetwork("mobilenetv2",NumClasses=numClasses);
+%net = replaceLayer(net,"Logits_softmax",layer);
 
 %net = imagePretrainedNetwork("resnet18",NumClasses=numClasses);
 %net = replaceLayer(net,"prob",layer);
@@ -11,27 +11,29 @@ net = replaceLayer(net,"Logits_softmax",layer);
 %net = imagePretrainedNetwork("resnet50",NumClasses=numClasses);
 %net = replaceLayer(net,"fc1000_softmax",layer);
 
-%net = imagePretrainedNetwork("resnet101",NumClasses=numClasses);
-%net = replaceLayer(net,"prob",layer);
+net = imagePretrainedNetwork("resnet101",NumClasses=numClasses);
+net = replaceLayer(net,"prob",layer);
 
 %analyzeNetwork(net);
 
-net = freezeNetwork(net,LayerNamesToIgnore="Logits"); % pour mobilenetv2
-%net = freezeNetwork(net,LayerNamesToIgnore="fc1000"); % pour resnet18/resnet50/resnet101
+%net = freezeNetwork(net,LayerNamesToIgnore="Logits"); % pour mobilenetv2
+net = freezeNetwork(net,LayerNamesToIgnore="fc1000"); % pour resnet18/resnet50/resnet101
 
 miniBatchSize = 64;
 miniBatchPerEpoch = floor(numTrain/miniBatchSize);
 valFreq = floor(miniBatchPerEpoch * 0.5);
 
-%metric = accuracyMetric(ClassificationMode="multilabel", AverageType="macro");
-%metric = precisionMetric(ClassificationMode="multilabel", AverageType="macro");
-metric = fScoreMetric(ClassificationMode="multilabel", AverageType="macro");
+accuracy = accuracyMetric(ClassificationMode="multilabel", AverageType="macro");
+precision = precisionMetric(ClassificationMode="multilabel", AverageType="macro");
+fscore = fScoreMetric(ClassificationMode="multilabel", AverageType="macro");
+metric = {fscore};
 
 options = trainingOptions("adam", ...
-    InitialLearnRate=0.001, ...
+    InitialLearnRate=0.005, ...
     LearnRateSchedule="piecewise", ...
-    LearnRateDropFactor=0.2, ...
-    LearnRateDropPeriod=5, ...
+    LearnRateDropFactor=0.5, ...
+    LearnRateDropPeriod=3, ...
+    L2Regularization=0.0001,...
     MiniBatchSize=miniBatchSize, ...
     MaxEpochs= 10, ...
     Verbose= false, ...
